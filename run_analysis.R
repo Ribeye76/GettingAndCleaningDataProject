c8aa1e9b55cbbb629abb27a8848e5553bdef221e
Jose Maria Rivas Carvalho <jose.rivas.mx@gmail.com>
2016-06-17 13:43:54 -0500

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,4 +1,4 @@
-## Download and unzip the dataset
+## Download and unzip the dataset if it does not already exist
 url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 archivo <- "datos.zip"
 if (!file.exists("datos.zip")){
@@ -7,4 +7,11 @@ if (!file.exists("datos.zip")){
 if (!file.exists("UCI HAR Dataset")) { 
   unzip(archivo) 
   }
+
+# Load activity labels and features
+activitylabels <- read.table("UCI HAR Dataset/activity_labels.txt")
+activitylabels[,2] <- as.character(activitylabels[,2])
+features <- read.table("UCI HAR Dataset/features.txt")
+features[,2] <- as.character(features[,2])
+
 
