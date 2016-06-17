967d2f94e904f66ecad39c899281672aeb487104
Jose Maria Rivas Carvalho <jose.rivas.mx@gmail.com>
2016-06-17 14:05:45 -0500

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -8,10 +8,15 @@ if (!file.exists("UCI HAR Dataset")) {
   unzip(archivo) 
   }
 
-# Load activity labels and features
+# Load the activity and feature info
 activitylabels <- read.table("UCI HAR Dataset/activity_labels.txt")
 activitylabels[,2] <- as.character(activitylabels[,2])
 features <- read.table("UCI HAR Dataset/features.txt")
 features[,2] <- as.character(features[,2])
 
-
+# Extract only the data on mean and standard deviation
+featuresMeanStd <- grep(".*mean.*|.*std.*", features[,2])
+featuresMeanStd.names <- features[featuresMeanStd,2]
+featuresMeanStd.names = gsub('-mean', 'Mean', featuresMeanStd.names)
+featuresMeanStd.names = gsub('-std', 'Std', featuresMeanStd.names)
+featuresMeanStd.names <- gsub('[-()]', '', featuresMeanStd.names)
